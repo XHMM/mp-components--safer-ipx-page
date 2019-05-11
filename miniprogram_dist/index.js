4955894a26838f71d83260a05d597240b3b3e55c
xhmm <super.xhmm@gmail.com>
2019-05-11 22:05:25 +0800

    fixed height value not work when not ipx

--- miniprogram_dist/index.js
+++ miniprogram_dist/index.js
@@ -96,37 +96,23 @@ Component({
   properties: {
     pageHeight: {
       type: String,
-      value: '100vh' /* ,
-                     observer(newVal, oldVal, changedPath) {
-                     console.log(newVal, oldVal, changedPath)
-                     if (newVal.startsWith('calc(')) {
-                     console.log('pageHeight type: calc')
-                     this.setData({
-                     pageHeightType: 'calc'
-                     })
-                     } else if (newVal.startsWith('var(')) {
-                     console.log('pageHeight type: var')
-                     this.setData({
-                     pageHeightType: 'var'
-                     })
-                     } else {
-                     console.log('pageHeight type: normal')
-                     }
-                     } */
+      value: "100vh"
+      /* observer(newVal, oldVal, changedPath) {
+        console.log(newVal, oldVal, changedPath)
+      } */
     }
   },
   data: {
-    pageHeightType: 'normal', // normal即普通值比如100px var即css variable calc即calc(x - x)
     isIpx: false
   },
-  externalClasses: ['safer-ipx-page-class'],
+  externalClasses: ["safer-ipx-page-class"],
   lifetimes: {
     attached: function attached() {
       var _this = this;
 
       wx.getSystemInfo({
         success: function success(res) {
-          if (res.model === 'iPhone X') _this.setData({ isIpx: true });
+          if (res.model === "iPhone X") _this.setData({ isIpx: true });
         },
         fail: function fail(err) {
           console.log(err);
--- src/index.js
+++ src/index.js
@@ -2,40 +2,26 @@ Component({
   properties: {
     pageHeight: {
       type: String,
-      value: '100vh'/* ,
-      observer(newVal, oldVal, changedPath) {
+      value: "100vh"
+      /* observer(newVal, oldVal, changedPath) {
         console.log(newVal, oldVal, changedPath)
-        if (newVal.startsWith('calc(')) {
-          console.log('pageHeight type: calc')
-          this.setData({
-            pageHeightType: 'calc'
-          })
-        } else if (newVal.startsWith('var(')) {
-          console.log('pageHeight type: var')
-          this.setData({
-            pageHeightType: 'var'
-          })
-        } else {
-          console.log('pageHeight type: normal')
-        }
       } */
     }
   },
   data: {
-    pageHeightType: 'normal', // normal即普通值比如100px var即css variable calc即calc(x - x)
     isIpx: false
   },
-  externalClasses: ['safer-ipx-page-class'],
+  externalClasses: ["safer-ipx-page-class"],
   lifetimes: {
     attached() {
       wx.getSystemInfo({
-        success: (res) => {
-          if (res.model === 'iPhone X') this.setData({isIpx: true})
+        success: res => {
+          if (res.model === "iPhone X") this.setData({ isIpx: true });
         },
-        fail: (err => {
-          console.log(err)
-        })
-      })
+        fail: err => {
+          console.log(err);
+        }
+      });
     }
   }
-})
+});
